169c1c1bef4b35d848a12afa0663410d93f29432
Shrestdtu <86075036+Shrestdtu@users.noreply.github.com>
2022-06-29 16:49:52 +0530

    Delete diamond.js

--- diamond.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("wmore").addEventListener("click",function(){
-    document.getElementById("f").style.backgroundImage="url('hi.jpeg')";
-})
-
-document.getElementById("subss").addEventListener("click",function(){
-    document.getElementById("f").style.backgroundImage= "url('1487187.jpeg')";
-})
